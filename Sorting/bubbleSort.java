bb9992076b02d620394dd80f50c14eab56440cd0
Akaash B <akaashandrew1@gmail.com>
2024-03-09 23:43:01 +0530

    bubble sort addded

---
+++ Sorting/bubbleSort.java
@@ -0,0 +1,50 @@
+package Sorting;
+
+import java.util.*;
+public class bubbleSort{
+    public static void main(String[] args) {
+        int arr[]={10, 5, 3, 8, 15, 7, 20, 18, 12, 25, 6, 2, 30, 22, 11, 14};
+        // int arr[]={1,2,3,4};
+        System.out.println(Arrays.toString(bubbleSortLogicLoop(arr, 0, arr.length-1)));
+        System.out.println(Arrays.toString(bubbleSortLogicRecursion(arr,0,arr.length-1)));
+    }
+
+    private static int[] bubbleSortLogicLoop(int[] arr, int start, int end) {
+        boolean swap;
+        while(start<=end){
+            swap=false;
+            for(int i=start;i<end;i++){
+                if(arr[i]>arr[i+1]){
+                    int temp=arr[i];
+                    arr[i]=arr[i+1];
+                    arr[i+1]=temp;
+                    swap=true;
+                }
+            }
+            --end;
+            if(!swap){
+                break;
+            }
+        }
+        return arr;
+    }
+
+    private static int[] bubbleSortLogicRecursion(int[] arr,int start,int end) {
+        if(start>=end){
+            return arr;
+        }
+        if(arr[start]>arr[start+1]){
+            int temp=arr[start];
+            arr[start]=arr[start+1];
+            arr[start+1]=temp;
+        }
+        arr=bubbleSortLogicRecursion(arr, start+1, end);
+        return bubbleSortLogicRecursion(arr, start, end-1);
+    }
+}
+
+
+// In every pass, largest element moved to end
+// aka sinking sort,exchange sort,inplace sort
+// best case- O(N)
+// worst case- O(N pow 2);
